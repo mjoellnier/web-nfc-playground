ea7c52e4c949547e54ad8208617f4448bee98cf3
mjoellnier <other@max-wuest.de>
2020-04-18 18:18:18 +0200

    Updates

--- sw.js
+++ sw.js
@@ -13,15 +13,7 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/web-nfc-playground/precache-manifest.94d526514d8f266519c4a1712ea32a68.js"
-);
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.core.skipWaiting();
 
 workbox.core.clientsClaim();
 
@@ -30,10 +22,26 @@ workbox.core.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "1f7cfcf1d1608e993fcce4d103b4be3a"
+  },
+  {
+    "url": "static/css/main.3e9b0772.chunk.css",
+    "revision": "07997f9becbafaf04f5c7940973da986"
+  },
+  {
+    "url": "static/js/2.debdf233.chunk.js",
+    "revision": "4d8b1dd8ea675ca03a867cc668cbd111"
+  },
+  {
+    "url": "static/js/main.7fbbd31b.chunk.js",
+    "revision": "df2cdddb9f6842ffd49246d6f345b204"
+  },
+  {
+    "url": "static/js/runtime-main.e43c6462.js",
+    "revision": "1d1154a27825248a72242a2d7cfae328"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-nfc-playground/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
