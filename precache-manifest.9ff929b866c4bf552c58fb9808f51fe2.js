14d979f9c34f4e11d021a5f67d83045e5160f26d
mjoellnier <other@max-wuest.de>
2020-04-18 15:02:43 +0200

    Updates

--- precache-manifest.9ff929b866c4bf552c58fb9808f51fe2.js
+++ precache-manifest.9ff929b866c4bf552c58fb9808f51fe2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1b4e7778d30bb48cea5c26a22b2d50de",
+    "revision": "c66eac83cd7c36d224975044da0fa245",
     "url": "/web-nfc-playground/index.html"
   },
   {
-    "revision": "000a5a42c6ac4e7a2c49",
+    "revision": "28f4455cbd92e04c710d",
     "url": "/web-nfc-playground/static/css/main.99f4b851.chunk.css"
   },
   {
-    "revision": "ec6ccc835e5a669d33b8",
-    "url": "/web-nfc-playground/static/js/2.2261cb21.chunk.js"
+    "revision": "eaa7b43dcf679e1000b2",
+    "url": "/web-nfc-playground/static/js/2.e93d6871.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/web-nfc-playground/static/js/2.2261cb21.chunk.js.LICENSE.txt"
+    "url": "/web-nfc-playground/static/js/2.e93d6871.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "000a5a42c6ac4e7a2c49",
-    "url": "/web-nfc-playground/static/js/main.63ca2f61.chunk.js"
+    "revision": "28f4455cbd92e04c710d",
+    "url": "/web-nfc-playground/static/js/main.b9a11720.chunk.js"
   },
   {
     "revision": "7ffeb1be215d1f86ad44",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/web-nfc-playground/precache-manifest.48f4482a4dee0ee3b3e02dd57b0d1535.js"
+  "/web-nfc-playground/precache-manifest.9ff929b866c4bf552c58fb9808f51fe2.js"
 );
 
 self.addEventListener('message', (event) => {
